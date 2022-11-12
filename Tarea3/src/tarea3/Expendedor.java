21e9533434b9a90038623f9362fad0027d0f1009
JorgeAF23 <joavendano2021@udec.cl>
2022-11-12 17:01:40 -0300

    se agregan los botones a la clase ventana

--- Tarea3/src/tarea3/Expendedor.java
+++ Tarea3/src/tarea3/Expendedor.java
@@ -7,11 +7,10 @@ import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-class Expendedor extends JPanel{
+class Expendedor extends JPanel {
     public void paint(Graphics g){
         super.paint(g);
         ImageIcon ExpendedorImagen = new ImageIcon(getClass().getResource("/images/Expendedor.png"));
         g.drawImage(ExpendedorImagen.getImage(), 280, 20, 380,530,this);
-    }
-    
+    }  
 }
