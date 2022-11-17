20519e933a74f1500aa4bebf7d25e5fdecab8a28
Fer-1111 <fcorona2020@udec.cl>
2022-11-17 11:44:28 -0300

    monedas

---
+++ Tarea3/src/tarea3/ArrayMonedas.java
@@ -0,0 +1,26 @@
+
+
+package tarea3;
+
+
+import java.util.ArrayList;
+
+public class ArrayMonedas{
+    private final ArrayList<Moneda> monedasVuelto;
+
+    public ArrayMonedas() {
+        monedasVuelto = new ArrayList<>();
+    }   
+    
+    public void addMoneda(Moneda m){
+        monedasVuelto.add(m);
+    }
+
+    public int arrayLong(){
+        return monedasVuelto.size();
+    }
+    public String Monedas(int i){
+        return monedasVuelto.get(i).MonedaDevuelta();
+    }
+
+}
